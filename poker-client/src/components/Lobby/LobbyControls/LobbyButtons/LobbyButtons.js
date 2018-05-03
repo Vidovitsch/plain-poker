ef7072c7cc411affa8d51637f58867dfa900bfac
Vidovitsch <david.de.bekker@gmail.com>
2018-05-03 11:29:10 +0200

    Buttons on lobby console made clickable

--- poker-client/src/components/Lobby/LobbyControls/LobbyButtons/LobbyButtons.js
+++ poker-client/src/components/Lobby/LobbyControls/LobbyButtons/LobbyButtons.js
@@ -5,13 +5,18 @@ import './LobbyButtons.css';
 class LobbyButtons extends Component {
   constructor(props) {
     super(props);
+    this.createTable = this.createTable.bind(this);
+  }
+
+  createTable() {
+    alert('table created');
   }
 
   render() {
     return (
       <div className="LobbyButtons">
         <button>Join</button><br />
-        <button>New</button>
+        <button onClick={this.createTable} >New</button>
       </div>
     );
   }
